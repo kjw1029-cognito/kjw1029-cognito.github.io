8a75314ee64acd18f0695404bd4a47bfb43b2619
Jangwook Kim <kim.jangwook@connecty.co.jp>
2019-07-08 10:52:15 +0900

    first init

---
+++ _nuxt/94f75a0f359852d9ab13.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{143:function(e,t,n){"use strict";n.r(t);var r=n(23),component=Object(r.a)({},function(){this.$createElement;this._self._c;return this._m(0)},[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("h1",[this._v("This is Registered Page")])])}],!1,null,null,null);t.default=component.exports}}]);
